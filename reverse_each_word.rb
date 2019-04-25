e501ca84dde54d893e04e13f2a566752b46d360e
Brian Salemi <briandsalemi@gmail.com>
2019-04-25 15:20:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   my_array = string.split
   reversed_array = []
   my_array.each do |word|
-    puts reversed_array << word.reverse
+    reversed_array << word.reverse
   end
-  puts reversed_array
+   reversed_array
 end
